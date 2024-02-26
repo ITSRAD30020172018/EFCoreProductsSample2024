dc38abad6661f40409bee01ccb3ceb893a1930cb
Paul Powell <paul.powell@atu.ie>
2024-02-26 20:21:41 +0000

    Building out Blazor branch functionality

---
+++ ProductBlazorApp/Pages/ProductDetail.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using ProductModel;
+
+namespace ProductBlazorApp.Pages
+{
+    public partial class ProductDetail
+    {
+        IEnumerable<Product> products;
+        [Parameter]
+        public int ID { get; set; }
+        public Product Product { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            InitialiseProducts();
+            Product = products.FirstOrDefault(p => p.ID == ID);
+            return base.OnInitializedAsync();
+        }
+
+        private void InitialiseProducts()
+        {
+            products = new List<Product>
+            {
+                new Product{ ID = 1, Description = "Chai", StockOnHand = 12},
+                new Product{ ID = 2, Description = "Syrup", StockOnHand = 10},
+                new Product{ ID = 3, Description = "Cajun Seasoning", StockOnHand = 200},
+            };
+        }
+    }
+}
---
+++ ProductBlazorApp/Pages/ProductOverview.razor.cs
@@ -0,0 +1,24 @@
+﻿using ProductModel;
+
+namespace ProductBlazorApp.Pages
+{
+    public partial class ProductOverview
+    {
+        IEnumerable<Product> products;
+        protected override Task OnInitializedAsync()
+        {
+            InitialiseProducts();
+            return base.OnInitializedAsync();
+        }
+        private void InitialiseProducts()
+        {
+            products = new List<Product>
+            {
+                new Product{ ID = 1, Description = "Chai", StockOnHand = 12},
+                new Product{ ID = 2, Description = "Syrup", StockOnHand = 10},
+                new Product{ ID = 3, Description = "Cajun Seasoning", StockOnHand = 200},
+
+            };
+        }
+    }
+}
